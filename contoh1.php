7621a6b636ae413f542d218c5ccbe2598a8443e2
Sintaniaa <145775370+Sintaniaa@users.noreply.github.com>
2023-10-10 20:39:15 +0700

    FIle Controller - Pertemuan 2

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Sinta Nia
+            saya tinggal di daerah Jakarta Barat
+            olahraga yang saya sukai adalah lari";
+    }
+}
